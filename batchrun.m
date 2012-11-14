e7200d41ded5797acb1cd7c2dd0729179378bae3
Srivas Chennu <srivas@gmail.com>
2012-11-14 00:00:11 +0000

    Checked in Wed Nov 14 00:00:11 GMT 2012
    git-svn-id: file:///Users/chennu/Dropbox/Sources/P300Words@8 a8bdd232-2f15-4194-8166-6847383e6271

--- batchrun.m
+++ batchrun.m
@@ -1,15 +1,27 @@
 function batchrun(listnum)
 
+loadpaths
 loadsubj
+
 
 subjlist = subjlists{listnum};
 
 for s = 1:length(subjlist)
     basename = subjlist{s};
+    batchres{s,1} = basename;
+
+%     dataimport(basename);
+%     epochdata(basename,1);
     
-    dataimport(basename);
-    epochdata(basename);
+%    rejartifacts2([basename '_epochs'],1,4);
     
-    %rejartifacts2(basename,2,3);
-    
+    computeic([basename '_epochs']);
+%     filenames = dir(sprintf('%s%s*', filepath, basename));
+%     mfffiles = filenames(logical(cell2mat({filenames.isdir})));
+%     filename = mfffiles.name;
+%     info = read_mff_info([filepath filename]);
+%     
+%     batchres{s,2} = info.date;
 end
+% [~,sortidx] = sort(batchres(:,2));
+% batchres(sortidx,:)
--- loadsubj.m
+++ loadsubj.m
@@ -12,30 +12,28 @@ subjlists = {
     'p0112_p300words'
     'p0212_p300words'
     'p0312_p300words'
-    'p0412_p300words'
+%     'p0412_p300words' %too few trials
 %     'p0510V2_p300words' %bad
 %     'p0710V2_p300words' %bad
 %     'p0711_p300words' %bad
     'p0811_p300words'
-    'p0911_p300words'
     'p0911_p300words'
     'p1011_p300words'
     'p1211_p300words'
     'p1311_p300words'
     'p1411_p300words'
     'p1511_p300words'
-    'p1611_p300words'
+    'p1611_p300words' %clean
     'p1711_p300words'
 %     'p66v4_p300words' %bad
-    
-    'p1811_p300words'
-    'p0411V2_p300words'
-    'p2111_p300words'
-    'p2011_p300words'
-    'p1911_p300words'
-    'p0512_p300words'
+    'p1811_p300words' %clean
+    'p0411V2_p300words' %noisy
+%     'p2111_p300words' %too few trials
+    'p2011_p300words' %noisy
+    'p1911_p300words' %noisy
+    'p0512_p300words' %noisy
     'p0511v2_p300words'
-    'p0311v2_p300words'
+%     'p0311v2_p300words' %bad
     'p211v2_p300words'
     };
     };
